de499d99271f640e28d6b251baf61b2b09a904bc
zhangxm <zhangxm@youpu.me>
2017-04-21 11:13:05 +0800

    后台接口修改

--- application/library/enum/System.php
+++ application/library/enum/System.php
@@ -4,9 +4,9 @@ class Enum_System {
 
     const RPC_REQUEST_PACKAGE = 'ia';
 
-    const SYSTEM_NAME = '游谱OTA后台系统';
+    const SYSTEM_NAME = 'EASYISERVICE管理后台';
 
-    const SERVICE_API_DOMAIN = 'http://api-dev.easyiservice.com';
+    const SERVICE_API_DOMAIN = 'http://testapi.liheinfo.com';
 
     public static function getServiceApiUrlByLink($url){
         $url = strpos('http', $url) ? $url : self::SERVICE_API_DOMAIN . $url;
